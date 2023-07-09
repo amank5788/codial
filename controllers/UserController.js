0a87c610bcf9f001365a2866ce48f84b30a98596
amank5788 <88306289+amank5788@users.noreply.github.com>
2023-07-09 17:09:31 +0530

    manual auth in different branch

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,25 @@ const User=require('../models/userSchema');
 
 
 module.exports.profile =function(req,res){
-    return  res.end("<h1> users profile</h1>");
+  if(req.cookies.user_id){
+   User.findById(req.cookies.user_id).then((user)=>{
+    console.log(`you are logged in: ${user.name}`);
+    return res.render('profile',{
+      name: user.name
+
+    });
+   }).catch((err)=>{
+    console.log(`cookies id not found ${err}`);
+    return res.redirect('/users/signin');
+   }); 
+ 
+}
+else{
+  console.log('cookies is not there',req.cookies);
+  return res.redirect('/users/signin');
+}
+
+    
   };
 
 module.exports.post =function(req,res){
@@ -40,5 +58,31 @@ module.exports.post =function(req,res){
    });
   };
 
+  module.exports.create_session =function(req,res){
+    
+   User.findOne({email: req.body.email}).then((user)=>{
+    if(user && user.password==req.body.password){
+      console.log(`you are logged in : ${user.name}`);
+      res.cookie(
+        'user_id',user._id);
+       
+      return res.render('profile',{
+        name: user.name
+      });
+    }
+    return res.redirect('back');
+   }).catch((err)=>{
+    if(err){
+      console.log(`error in finding user while signing up: ${err}`); 
+      return;
+    }
+   });
+  };
 
+  module.exports.Signout=function(req,res){
+    
+    res.clearCookie('user_id');
+    
+    return res.redirect('/users/signin');
+  }
   
--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,9 @@ router.get('/profile',UserController.profile);
 router.get('/post',UserController.post);
 router.get('/signin', UserController.Signin);
 router.get('/signup', UserController.Signup);
+router.get('/signout',UserController.Signout);
 router.post('/signup_now', UserController.Signup_now);
-
+router.post('/create_session', UserController.create_session);
 
 
 module.exports =router;
